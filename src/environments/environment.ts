7648fabbc2424f55698e1dffad78f1131bf07c4e
rumsyia <rumsiya@gmail.com>
2026-01-08 13:25:37 +0530

    path check

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl : 'http://localhost:8000/api/',
-  cloudImage:'	https://res.cloudinary.com/drl1naxne/image/upload/v1767857179/'
+  cloudImage:'https://res.cloudinary.com/drl1naxne/image/upload/v1767857179/'
 
 };
 
